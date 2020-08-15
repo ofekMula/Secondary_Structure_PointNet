27386b21e5d3900eb5e3bd4d52b83c3092e7e95c
rickyBEN <ricky.benkovich@gmail.com>
2020-08-15 10:38:07 +0300

    Added 2 functions in Protein_Utils.py for save the db.

--- Protein_Data_Base/Protein_Utils.py
+++ Protein_Data_Base/Protein_Utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+import glob
+import os
+import sys
 from Protein_Data_Base import sample_code
 import os
 from Bio.PDB.DSSP import DSSP
@@ -5,66 +9,122 @@ import Bio.PDB
 import numpy as np
 from Bio.PDB import *
 
-##We get a protein like "1a09", a chain number like 'A'.
-##That we download the pdb file & parse it.
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(BASE_DIR)
+
+# -----------------------------------------------------------------------------
+# CONSTANTS
+# -----------------------------------------------------------------------------
+
+colors_dict = { 'H': [0, 255, 0],
+                'I': [0, 255, 0],
+                'G': [0, 255, 0],
+                'B': [0, 0, 255],
+                'E': [0, 0, 255],
+                'S': [255, 0, 0],
+                'T': [255, 0, 0],
+                '-': [50, 50, 50]  # None
+                }
+
+
+
+# -----------------------------------------------------------------------------
+# PROCESS PROTEINS
+# -----------------------------------------------------------------------------
+##  We get a protein like "1a09", a chain number like 'A'.
+##  That we download the pdb file & parse it.
 
 
 def download_and_parse_pdb(pdb_name, num_chain):
-    print ("parsing_file:", pdb_name, num_chain)
+    print("parsing_file:", pdb_name, num_chain)
     parser = PDBParser()
     pdbl = PDBList()
 
-    #Downloading the file
+    #  Downloading the file
     path = "/specific/netapp5_2/iscb/wolfson/yarinluhmany/v_env/dssp-master"
-    file_name = pdbl.retrieve_pdb_file(pdb_code = pdb_name, file_format = "pdb", pdir=path)
+    file_name = pdbl.retrieve_pdb_file(pdb_code=pdb_name, file_format="pdb", pdir=path)
     new_name = path + '/' + pdb_name + '.pdb'
     os.rename(file_name, new_name)
-    file_data_name = pdb_name+'.pdb'
+    file_data_name = pdb_name + '.pdb'
 
-    #Parsing the file
+    #  Parsing the file
     structure = parser.get_structure(pdb_name, file_data_name)
     chain_prot = structure[0][num_chain]
 
     return structure, chain_prot
 
-#create a num_py array of [x,y,z] coordinates for all atoms.
+
+#  create a num_py array of [x,y,z] coordinates for all atoms.
 def list_of_atom_coordinates(chain_prot):
     res = sample_code.process_chain(chain_prot)
     all_coordinates = res[2]
     list_of_atoms = [np.zeros(3)]
     for i in range(len(all_coordinates)):
         for j in range(len(all_coordinates[i])):
-            list_of_atoms = np.append(list_of_atoms,[all_coordinates[i][j]], axis=0)
+            list_of_atoms = np.append(list_of_atoms, [all_coordinates[i][j]], axis=0)
     return np.delete(list_of_atoms, 0, axis=0)
 
-#create a string of secondary structure for each atom.
+
+# create a string of secondary structure for each atom.
 def list_of_atom_colors(pdb_name, structure, num_chain, chain_prot):
-    file_data_name = pdb_name+'.pdb'
+    file_data_name = pdb_name + '.pdb'
     model = structure[0]
     dssp = DSSP(model, file_data_name, dssp='mkdssp')
     secondary_struct = ""
+    colors_array = np.array([])
+
     for i in range(len(list(dssp.keys()))):
-        if dssp.keys()[i][0]!=num_chain:
+        if dssp.keys()[i][0] != num_chain:
             continue
         residue_number = dssp.keys()[i][1][1]
-        #print("res_num:", residue_number)
-        #find relecant key in dictionary
-        a_key= list(dssp.keys())[i]
+        # print("res_num:", residue_number)
+        # find relecant key in dictionary
+        a_key = list(dssp.keys())[i]
         secondary_structure_of_residue = dssp[a_key][2]
-        #find how many atoms in this residue
+        # find how many atoms in this residue
         for residue in Selection.unfold_entities(chain_prot, 'R'):
             if (sample_code.is_residue(residue)):
                 if residue.get_id()[1] == residue_number:
                     num_of_atoms_in_residue = len([atom for atom in residue if sample_code.is_heavy_atom(atom)])
-                    secondary_struct+=num_of_atoms_in_residue * secondary_structure_of_residue
+                    secondary_struct += num_of_atoms_in_residue * secondary_structure_of_residue
+                    if secondary_structure_of_residue in colors_dict:
+                        colors_array = np.array(colors_array, colors_dict[secondary_structure_of_residue])  # it is an array
+                    # of arrays of size 3 to represent colors
+                    # every position in the array corresponds to atom coordinates in the same position
+                    else:
+                        colors_array = np.array(colors_array, colors_dict['-'])
                     break
-        #add to output
+        # add to output
 
-    return secondary_struct
+    return secondary_struct, colors_array
+
+
+# protein_path for example: Area_1/11as_A/Protein
+def save_to_numpy_file(protein_path, point_cord_color_array):
+    out_file_name = BASE_DIR + str.split(protein_path, '/')[1] + '.numpy'  # protein name : 11as_A.numpy
+    np.save(out_file_name, point_cord_color_array)
+
+
+def create_cord_color_array(coordinates, colors):
+    if len(coordinates) != len(colors):
+        print("ERROR - different number of coordinates and colors")
+    else:
+        arr = np.array([])
+        for i in range(0, len(coordinates)):
+            arr = np.array(arr, [coordinates[i], colors[i]])  # add current coordinates with there color
+        return arr
+
 
 if __name__ == "__main__":
+    protein_file_name = 'Area_1/11as_A/Protein'
     structure, chain_prot = download_and_parse_pdb("11as", 'A')
-    list_of_atoms=list_of_atom_coordinates(chain_prot)
+    list_of_atoms = list_of_atom_coordinates(chain_prot)
     print(list_of_atoms, "\nlen = ", len(list_of_atoms))
-    list_of_colors =list_of_atom_colors("11as", structure, 'A', chain_prot)
+    list_of_colors, colors_arr = list_of_atom_colors("11as", structure, 'A', chain_prot)
     print(list_of_colors, "\nlen = ", len(list_of_colors))
+
+    cord_color_arr = create_cord_color_array(list_of_atoms, colors_arr)
+    save_to_numpy_file(protein_file_name, cord_color_arr)
+
+
