c89b48719e7605be52f641376b52894da7037734
ofekMula <46421124+ofekMula@users.noreply.github.com>
2020-08-19 20:01:47 +0300

    comment out modelnet40 files

--- provider.py
+++ provider.py
@@ -9,12 +9,15 @@ sys.path.append(BASE_DIR)
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
+'''
+### it looks like the modelnet40 db is irrelevant for the training.
 if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
     www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
     zipfile = os.path.basename(www)
     os.system('wget %s --no-check-certificate; unzip %s' % (www, zipfile))
     os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
     os.system('rm %s' % (zipfile))
+'''
 
 ### should work well with np arrays
 def shuffle_data(data, labels):
