8394146905c811e49022b77b9f92cb3c586d4c99
rickyBEN <ricky.benkovich@gmail.com>
2020-08-24 20:32:25 +0300

    added normalizing functions

--- Protein_Data_Base/Protein_Utils.py
+++ Protein_Data_Base/Protein_Utils.py
@@ -4,7 +4,7 @@ import os
 import sys
 from Protein_Data_Base import sample_code
 import os
-from Bio.PDB.DSSP import DSSP
+# from Bio.PDB.DSSP import DSSP
 import Bio.PDB
 import numpy as np
 from Bio.PDB import *
@@ -17,15 +17,27 @@ sys.path.append(BASE_DIR)
 # CONSTANTS
 # -----------------------------------------------------------------------------
 
-colors_dict = { 'H': [0, 255, 0],
-                'I': [0, 255, 0],
-                'G': [0, 255, 0],
-                'B': [0, 0, 255],
-                'E': [0, 0, 255],
-                'S': [255, 0, 0],
-                'T': [255, 0, 0],
-                '-': [50, 50, 50]  # None
+label_dict = { 'H': 0,
+                'I': 1,
+                'G': 2,
+                'B': 3,
+                'E': 4,
+                'S': 5,
+                'T': 6,
+                '-': 7  # None
                 }
+
+residue_to_attributes = {'C' : np.array([28,7.4,26.3,40.3,7.4,0]),'D' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'S' : np.array([28,7.4,26.3,40.3,7.4,0]),'Q' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'K' : np.array([28,7.4,26.3,40.3,7.4,0]),'I' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'P' : np.array([28,7.4,26.3,40.3,7.4,0]),'T' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'F' : np.array([28,7.4,26.3,40.3,7.4,0]),'N' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'G' : np.array([28,7.4,26.3,40.3,7.4,0]),'H' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'L' : np.array([28,7.4,26.3,40.3,7.4,0]),'R' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'W' : np.array([28,7.4,26.3,40.3,7.4,0]),'A' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'V' : np.array([28,7.4,26.3,40.3,7.4,0]),'E' : np.array([28,7.4,26.3,40.3,7.4,0]),
+                         'Y' : np.array([28,7.4,26.3,40.3,7.4,0]),'M' : np.array([28,7.4,26.3,40.3,7.4,0])
+}
 
 
 
@@ -56,23 +68,29 @@ def download_and_parse_pdb(pdb_name, num_chain):
 
 
 #  create a num_py array of [x,y,z] coordinates for all atoms.
-def list_of_atom_coordinates(chain_prot):
+# def list_of_atom_coordinates(chain_prot):
+#     res = sample_code.process_chain(chain_prot)
+#     all_coordinates = res[2]
+#     list_of_atoms = [np.zeros(3)]
+#     for i in range(len(all_coordinates)):
+#         for j in range(len(all_coordinates[i])):
+#             list_of_atoms = np.append(list_of_atoms, [all_coordinates[i][j]], axis=0)
+#     return np.delete(list_of_atoms, 0, axis=0)
+
+
+#  create a num_py array of [x,y,z] coordinates for all atoms.
+def list_of_residue_coordinates_and_residue_seq(chain_prot):
     res = sample_code.process_chain(chain_prot)
-    all_coordinates = res[2]
-    list_of_atoms = [np.zeros(3)]
-    for i in range(len(all_coordinates)):
-        for j in range(len(all_coordinates[i])):
-            list_of_atoms = np.append(list_of_atoms, [all_coordinates[i][j]], axis=0)
-    return np.delete(list_of_atoms, 0, axis=0)
+    return res[0], res[1]
 
 
-# create a string of secondary structure for each atom.
-def list_of_atom_colors(pdb_name, structure, num_chain, chain_prot):
+# create a string of secondary structure for each residue.
+def list_of_residue_labels(pdb_name, structure, num_chain, chain_prot):
     file_data_name = pdb_name + '.pdb'
     model = structure[0]
     dssp = DSSP(model, file_data_name, dssp='mkdssp')
     secondary_struct = ""
-    colors_array = np.array([])
+    label_array = np.array([])
 
     for i in range(len(list(dssp.keys()))):
         if dssp.keys()[i][0] != num_chain:
@@ -86,18 +104,48 @@ def list_of_atom_colors(pdb_name, structure, num_chain, chain_prot):
         for residue in Selection.unfold_entities(chain_prot, 'R'):
             if (sample_code.is_residue(residue)):
                 if residue.get_id()[1] == residue_number:
-                    num_of_atoms_in_residue = len([atom for atom in residue if sample_code.is_heavy_atom(atom)])
-                    secondary_struct += num_of_atoms_in_residue * secondary_structure_of_residue
-                    if secondary_structure_of_residue in colors_dict:
-                        colors_array = np.array(colors_array, colors_dict[secondary_structure_of_residue])  # it is an array
-                    # of arrays of size 3 to represent colors
-                    # every position in the array corresponds to atom coordinates in the same position
+                    secondary_struct += secondary_structure_of_residue
+                    if secondary_structure_of_residue in label_dict:
+                        label_array = np.array(label_array, label_dict[secondary_structure_of_residue])  # label
+                    # every position in the array corresponds to residue coordinates in the same position
                     else:
-                        colors_array = np.array(colors_array, colors_dict['-'])
+                        label_array = np.array(label_array, label_dict['-'])
                     break
-        # add to output
 
-    return secondary_struct, colors_array
+    return secondary_struct, label_array
+
+# create a string of secondary structure for each atom.
+# def list_of_atom_colors(pdb_name, structure, num_chain, chain_prot):
+#     file_data_name = pdb_name + '.pdb'
+#     model = structure[0]
+#     dssp = DSSP(model, file_data_name, dssp='mkdssp')
+#     secondary_struct = ""
+#     colors_array = np.array([])
+#
+#     for i in range(len(list(dssp.keys()))):
+#         if dssp.keys()[i][0] != num_chain:
+#             continue
+#         residue_number = dssp.keys()[i][1][1]
+#         # print("res_num:", residue_number)
+#         # find relecant key in dictionary
+#         a_key = list(dssp.keys())[i]
+#         secondary_structure_of_residue = dssp[a_key][2]
+#         # find how many atoms in this residue
+#         for residue in Selection.unfold_entities(chain_prot, 'R'):
+#             if (sample_code.is_residue(residue)):
+#                 if residue.get_id()[1] == residue_number:
+#                     num_of_atoms_in_residue = len([atom for atom in residue if sample_code.is_heavy_atom(atom)])
+#                     secondary_struct += num_of_atoms_in_residue * secondary_structure_of_residue
+#                     if secondary_structure_of_residue in colors_dict:
+#                         colors_array = np.array(colors_array, colors_dict[secondary_structure_of_residue])  # it is an array
+#                     # of arrays of size 3 to represent colors
+#                     # every position in the array corresponds to atom coordinates in the same position
+#                     else:
+#                         colors_array = np.array(colors_array, colors_dict['-'])
+#                     break
+#         # add to output
+#
+#     return secondary_struct, colors_array
 
 
 # protein_path for example: Area_1/11as_A/Protein
@@ -106,25 +154,61 @@ def save_to_numpy_file(protein_path, point_cord_color_array):
     np.save(out_file_name, point_cord_color_array)
 
 
-def create_cord_color_array(coordinates, colors):
-    if len(coordinates) != len(colors):
+def create_cord_label_array(coordinates, labels):
+    if len(coordinates) != len(labels):
         print("ERROR - different number of coordinates and colors")
     else:
         arr = np.array([])
         for i in range(0, len(coordinates)):
-            arr = np.array(arr, [coordinates[i], colors[i]])  # add current coordinates with there color
+            arr = np.array(arr, [coordinates[i], labels[i]])  # add current coordinates with there color
         return arr
+
+## receives list_residues
+def normalize_coordinates(list_residues_coord):
+    xyz_max = np.amax(list_residues_coord, axis=0)[0:3]
+    list_residues_coord[:, 0:3] /= xyz_max
+    return list_residues_coord
+
+
+## receives the residues sequence and noralizes
+## zi = (xi - min(x))/(max(x)-min(x))
+def normalize_attributes(list_residues_sequence):
+    attribute_list = np.array([residue_to_attributes[c] for c in list_residues_sequence])
+    attribute_max = np.amax(attribute_list, axis=0)[0:6]
+    attribute_min = np.amin(attribute_list, axis=0)[0:6]
+
+    attribute_list[:, 0:6] -= attribute_min
+    attribute_list[:, 0:6] /= (attribute_max - attribute_min)
+    return attribute_list
+
+
+def normalize_full_attributes(list_residues_sequence, list_residues_coord):
+    attribute_list = normalize_attributes(list_residues_sequence)
+    list_residues_coord = normalize_coordinates(list_residues_coord)
+    if len(attribute_list) != len(list_residues_coord):
+        print("ERROR: Protein_Utils, normalize_full_attributes: attr len != len of coordinates")
+        return -1
+    else:
+        len_arr = len(attribute_list)
+        full_attr_array = np.array([np.concatenate((list_residues_coord[i], attribute_list[i])) for i in range(len_arr)])
+        return full_attr_array
+
+
+
 
 
 if __name__ == "__main__":
     protein_file_name = 'Area_1/11as_A/Protein'
     structure, chain_prot = download_and_parse_pdb("11as", 'A')
-    list_of_atoms = list_of_atom_coordinates(chain_prot)
-    print(list_of_atoms, "\nlen = ", len(list_of_atoms))
-    list_of_colors, colors_arr = list_of_atom_colors("11as", structure, 'A', chain_prot)
-    print(list_of_colors, "\nlen = ", len(list_of_colors))
+    list_of_residues, list_residue_coord = list_of_residue_coordinates_and_residue_seq(chain_prot)
+    print(list_of_residues + " " + list_residue_coord)
+    # list_of_atoms = list_of_atom_coordinates(chain_prot)
+    # print(list_of_atoms, "\nlen = ", len(list_of_atoms))
+    # list_of_colors, colors_arr = list_of_atom_colors("11as", structure, 'A', chain_prot)
+    # print(list_of_colors, "\nlen = ", len(list_of_colors))
+    #
+    # cord_color_arr = create_cord_color_array(list_of_atoms, colors_arr)
+    # save_to_numpy_file(protein_file_name, cord_color_arr)
 
-    cord_color_arr = create_cord_color_array(list_of_atoms, colors_arr)
-    save_to_numpy_file(protein_file_name, cord_color_arr)
 
 
