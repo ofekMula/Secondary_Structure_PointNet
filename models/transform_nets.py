d27c25824ef6cc7ed7df7f3baa952b2d05a24d6c
yarinluh <69578786+yarinluh@users.noreply.github.com>
2020-09-02 11:28:43 +0300

    Updating transform_nets.py
    divide layer sizes by 4.

--- models/transform_nets.py
+++ models/transform_nets.py
@@ -15,15 +15,15 @@ def input_transform_net(point_cloud, is_training, bn_decay=None, K=3):
     num_point = point_cloud.get_shape()[1].value
 
     input_image = tf.expand_dims(point_cloud, -1)
-    net = tf_util.conv2d(input_image, 64, [1,3],
+    net = tf_util.conv2d(input_image, 64 // 4, [1,3],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='tconv1', bn_decay=bn_decay)
-    net = tf_util.conv2d(net, 128, [1,1],
+    net = tf_util.conv2d(net, 128 // 4, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='tconv2', bn_decay=bn_decay)
-    net = tf_util.conv2d(net, 1024, [1,1],
+    net = tf_util.conv2d(net, 1024 // 4, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='tconv3', bn_decay=bn_decay)
@@ -31,14 +31,14 @@ def input_transform_net(point_cloud, is_training, bn_decay=None, K=3):
                              padding='VALID', scope='tmaxpool')
 
     net = tf.reshape(net, [batch_size, -1])
-    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
+    net = tf_util.fully_connected(net, 512 // 4, bn=True, is_training=is_training,
                                   scope='tfc1', bn_decay=bn_decay)
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
+    net = tf_util.fully_connected(net, 256 // 4, bn=True, is_training=is_training,
                                   scope='tfc2', bn_decay=bn_decay)
 
     with tf.variable_scope('transform_XYZ') as sc:
         assert(K==3)
-        weights = tf.get_variable('weights', [256, 3*K],
+        weights = tf.get_variable('weights', [256 // 4, 3*K],
                                   initializer=tf.constant_initializer(0.0),
                                   dtype=tf.float32)
         biases = tf.get_variable('biases', [3*K],
@@ -59,15 +59,15 @@ def feature_transform_net(inputs, is_training, bn_decay=None, K=64):
     batch_size = inputs.get_shape()[0].value
     num_point = inputs.get_shape()[1].value
 
-    net = tf_util.conv2d(inputs, 64, [1,1],
+    net = tf_util.conv2d(inputs, 64 // 4, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='tconv1', bn_decay=bn_decay)
-    net = tf_util.conv2d(net, 128, [1,1],
+    net = tf_util.conv2d(net, 128 // 4, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='tconv2', bn_decay=bn_decay)
-    net = tf_util.conv2d(net, 1024, [1,1],
+    net = tf_util.conv2d(net, 1024 // 4, [1,1],
                          padding='VALID', stride=[1,1],
                          bn=True, is_training=is_training,
                          scope='tconv3', bn_decay=bn_decay)
@@ -75,13 +75,13 @@ def feature_transform_net(inputs, is_training, bn_decay=None, K=64):
                              padding='VALID', scope='tmaxpool')
 
     net = tf.reshape(net, [batch_size, -1])
-    net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
+    net = tf_util.fully_connected(net, 512 // 4, bn=True, is_training=is_training,
                                   scope='tfc1', bn_decay=bn_decay)
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
+    net = tf_util.fully_connected(net, 256 // 4, bn=True, is_training=is_training,
                                   scope='tfc2', bn_decay=bn_decay)
 
     with tf.variable_scope('transform_feat') as sc:
-        weights = tf.get_variable('weights', [256, K*K],
+        weights = tf.get_variable('weights', [256 // 4, K*K],
                                   initializer=tf.constant_initializer(0.0),
                                   dtype=tf.float32)
         biases = tf.get_variable('biases', [K*K],
